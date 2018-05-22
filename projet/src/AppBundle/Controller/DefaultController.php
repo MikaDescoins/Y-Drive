ce4dc4b58a0cec69675caa19704122cb7d0b044b
Mika Descoins <mikadescoins@MacBook-Pro-de-Mika.local>
2018-05-22 16:56:35 +0200

    Correctif Panier / Bricolage pagination / Affichage produit

--- projet/src/AppBundle/Controller/DefaultController.php
+++ projet/src/AppBundle/Controller/DefaultController.php
@@ -9,38 +9,85 @@ use AppBundle\Document\Product;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 
 class DefaultController extends Controller
 {
 
     public function indexAction(Request $request)
     {
-        $product = $this->get('doctrine_mongodb')
-            ->getRepository('AppBundle:Product')
-            ->findAll();
 
-        if (!$product) {
-            throw $this->createNotFoundException('No product found for id '.$id);
+
+
+        $products = $this->get('doctrine_mongodb')
+            ->getManager()
+            ->createQueryBuilder('AppBundle:Product')
+
+            ->limit(100)
+            ->skip(0)
+
+            ->getQuery()
+            ->execute();
+
+        if($request->get('start')){
+            $products = $this->get('doctrine_mongodb')
+                ->getManager()
+                ->createQueryBuilder('AppBundle:Product')
+
+                ->limit(100)
+                ->skip($request->get('start'))
+
+                ->getQuery()
+                ->execute();
         }
 
-        print_r($product); die();
 
-        return 'AAA';
-        // do something, like pass the $product object into a template
+
+
+
+        return $this->render('products/index.html.twig',array(
+            'products'=>$products,
+        ));
+
+
+
+
     }
 
     public function createAction()
     {
-        $product = new Product();
-        $product->setName('A Foo Bar');
-        $product->setPrice('19.99');
 
-        $dm = $this->get('doctrine_mongodb')->getManager();
-        $dm->persist($product);
-        $dm->flush();
 
-        return new Response('Created product id '.$product->getId());
+        return $this->render('products/create.html.twig');
     }
+
+
+    public function createSubmitAction(Request $request)
+    {
+        if($request->getMethod() == "POST"){
+            $product = new Product();
+            $product->setProductId($request->get('productId'));
+            $product->setProductName($request->get('productName'));
+            $product->setRetailPrice($request->get('retailPrice'));
+            $product->setDescription($request->get('description'));
+
+            $dm = $this->get('doctrine_mongodb')->getManager();
+            $dm->persist($product);
+            $dm->flush();
+
+            $this->addFlash("success", "Le produit a été ajouté avec succès.");
+
+            return $this->redirectToRoute('ydrive_product_index');
+        }
+        else{
+            return new Response('Erreur, la création à échouée.');
+        }
+
+        
+    }
+
+
 
     public function showAction($id)
     {
@@ -52,9 +99,9 @@ class DefaultController extends Controller
             throw $this->createNotFoundException('No product found for id '.$id);
         }
 
-        print_r($product); die();
-
-        return 'AAA';
+        return $this->render('products/show.html.twig',array(
+            'product'=>$product,
+        ));
         // do something, like pass the $product object into a template
     }
 
@@ -62,16 +109,126 @@ class DefaultController extends Controller
     public function updateAction($id)
     {
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $product = $dm->getRepository('AcmeStoreBundle:Product')->find($id);
+        $product = $dm->getRepository('AppBundle:Product')->find($id);
 
         if (!$product) {
             throw $this->createNotFoundException('No product found for id '.$id);
         }
 
-        $product->setName('New product name!');
+
+
+        return $this->render('products/update.html.twig',array(
+            'product'=>$product,
+        ));
+    }
+
+    public function updateSubmitAction(Request $request, $id )
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $product = $dm->getRepository('AppBundle:Product')->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+
+        $product->setProductId($request->get('productId'));
+        $product->setProductName($request->get('productName'));
+        $product->setPrice($request->get('retailPrice'));
+        $product->setDescription($request->get('description'));
+        $product->setStock($request->get('stock'));
         $dm->flush();
 
-        return $this->redirectToRoute('homepage');
+        return $this->redirectToRoute('ydrive_product_index');
     }
+
+    public function deleteAction( $id )
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $product = $dm->getRepository('AppBundle:Product')->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+
+        $dm->remove($product);
+        $dm->flush();
+
+        return $this->redirectToRoute('ydrive_product_index');
+    }
+
+    public function addCartAction( $id )
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $product = $dm->getRepository('AppBundle:Product')->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+
+        $prdId = $product->getId();
+        $session = $this->container->get('session');
+        $session->set('cart', $session->get('cart'). '/'.$prdId);
+
+        $this->addFlash("success", "Le produit a bien été ajouté au panier.");
+
+
+
+
+        return $this->redirectToRoute('ydrive_product_index');
+    }
+
+    public function cartIndexAction( )
+    {
+        $session = $this->container->get('session');
+
+        $a = $session->get('cart');
+
+        $a = explode('/', $a);
+
+        $products = array();
+        $dm = $this->get('doctrine_mongodb')->getManager();
+
+        foreach ($a as $id){
+            $product = $dm->getRepository('AppBundle:Product')->find($id);
+            array_push($products, $product)   ;
+        }
+
+
+
+
+
+
+
+        return $this->render('cart/index.html.twig',array(
+            'products'=>$products,
+        ));
+    }
+
+
+
+
+    public function removeCartAction( $id )
+    {
+        $session = $this->container->get('session');
+
+        $a = $session->get('cart');
+
+        $a = explode('/', $a);
+
+        //$a = str_replace($id, "", $a);
+        $from = '/'.preg_quote($id, '/').'/';
+        $to ='';
+        $a =  preg_replace($from, $to, $a, 1);
+
+        $session->set('cart', implode("/", $a));
+
+
+
+        $this->addFlash("success", "Le produit a bien été retiré du panier.");
+
+        return $this->redirectToRoute('ydrive_cart_index');
+    }
+
+
 
 }
--- projet/src/AppBundle/Document/Product.php
+++ projet/src/AppBundle/Document/Product.php
@@ -4,6 +4,7 @@ namespace AppBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Doctrine\ODM\MongoDB\Mapping\Annotations\Id;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 
 /**
@@ -16,15 +17,33 @@ class Product
      */
     protected $id;
 
+
+    /**
+     * @MongoDB\Field(type="integer")
+     */
+    protected $product_id;
+
     /**
      * @MongoDB\Field(type="string")
      */
-    protected $name;
+    protected $product_name;
 
     /**
      * @MongoDB\Field(type="float")
      */
     protected $price;
+
+    /**
+     * @MongoDB\Field(type="integer")
+     */
+    protected $stock;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $description;
+
+    
 
     /**
      * Get id
@@ -37,32 +56,120 @@ class Product
     }
 
     /**
-     * Set name
+     * Set productId
      *
-     * @param string $name
-     * @return $this
+     * @param integer $productId
+     * @return self
      */
-    public function setName($name)
+    public function setProductId($productId)
     {
-        $this->name = $name;
+        $this->product_id = $productId;
         return $this;
     }
 
     /**
-     * Get name
+     * Get productId
      *
-     * @return string $name
+     * @return integer $productId
      */
-    public function getName()
+    public function getProductId()
     {
-        return $this->name;
+        return $this->product_id;
+    }
+
+    /**
+     * Set productName
+     *
+     * @param string $productName
+     * @return self
+     */
+    public function setProductName($productName)
+    {
+        $this->product_name = $productName;
+        return $this;
+    }
+
+    /**
+     * Get productName
+     *
+     * @return string $productName
+     */
+    public function getProductName()
+    {
+        return $this->product_name;
+    }
+
+    /**
+     * Set retailPrice
+     *
+     * @param float $retailPrice
+     * @return self
+     */
+    public function setRetailPrice($retailPrice)
+    {
+        $this->retail_price = $retailPrice;
+        return $this;
+    }
+
+    /**
+     * Get retailPrice
+     *
+     * @return float $retailPrice
+     */
+    public function getRetailPrice()
+    {
+        return $this->retail_price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return self
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string $description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set stock
+     *
+     * @param integer $stock
+     * @return self
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+        return $this;
+    }
+
+    /**
+     * Get stock
+     *
+     * @return integer $stock
+     */
+    public function getStock()
+    {
+        return $this->stock;
     }
 
     /**
      * Set price
      *
      * @param float $price
-     * @return $this
+     * @return self
      */
     public function setPrice($price)
     {
@@ -79,4 +186,9 @@ class Product
     {
         return $this->price;
     }
+
+
+
+
+
 }
